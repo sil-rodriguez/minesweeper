58fcc5acf5541a1886a2b37bbdf01b488953c76d
sil-rodriguez <63976639+sil-rodriguez@users.noreply.github.com>
2020-04-26 18:09:53 -0300

    create map api integration

--- minesweeper-ui/src/containers/map.js
+++ minesweeper-ui/src/containers/map.js
@@ -2,18 +2,16 @@
 
 import React, { Component } from "react";
 import Cell from "./cell";
-import {
-  nestedArray,
-  populateNestedArray,
-  valsAdjacentCounts
-} from "../helpers";
+import {populateMap} from "../helpers";
 
 type Props = {};
 type State = {
   mapSize: number,
   bombCount: number,
   theMap: Array<Array<number | string>>,
-  cellsClicked: number
+  cellsClicked: number,
+  isLoaded: boolean,
+  error: string
 };
 
 class Map extends Component<Props, State> {
@@ -24,13 +22,28 @@ class Map extends Component<Props, State> {
     this.state = {
       mapSize,
       bombCount,
-      theMap: valsAdjacentCounts(
-        populateNestedArray(nestedArray(mapSize, mapSize), "☀", bombCount),
-        "☀"
-      ),
       cellsClicked: 1
     };
   }
+"☀"
+  componentDidMount() {
+      fetch("http://localhost:8080/cells")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              theMap: populateMap(result)
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+      }
 
   incCellsClicked() {
     let { mapSize, bombCount, cellsClicked } = this.state;
@@ -42,31 +55,38 @@ class Map extends Component<Props, State> {
   }
 
   render() {
-    return (
-      <div>
-        <table>
-          <tbody>
-            {this.state.theMap.map((item, row) => {
-              return (
-                <tr key={row} className="mapRow">
-                  {item.map((subitem, col) => {
-                    return (
-                      <Cell
-                        key={col}
-                        row={row}
-                        column={col}
-                        value={subitem}
-                        incCellsClicked={this.incCellsClicked.bind(this)}
-                      />
-                    );
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
+    const { error, isLoaded} = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <div>
+          <table>
+            <tbody>
+              {this.state.theMap.map((item, row) => {
+                return (
+                  <tr key={row} className="mapRow">
+                    {item.map((subitem, col) => {
+                      return (
+                        <Cell
+                          key={col}
+                          row={row}
+                          column={col}
+                          value={subitem}
+                          incCellsClicked={this.incCellsClicked.bind(this)}
+                        />
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
   }
 }
 
